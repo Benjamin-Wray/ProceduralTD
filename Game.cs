e1dd17cc27c718ddf91a10f2fa96dff73270b42a
JustOnesAndZeros <benwray2005@gmail.com>
2022-07-21 21:32:21 +0100

    added 2D camera

---
+++ Game.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ProceduralTD
+{
+    public class Game : Microsoft.Xna.Framework.Game
+    {
+        private readonly GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        
+        private const int WindowWidth = 1280;
+        private const int WindowHeight = 720;
+
+        private float[,] _heightMap;
+
+        private Texture2D _pixel;
+
+        public Game()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = WindowWidth;
+            _graphics.PreferredBackBufferHeight = WindowHeight;
+            _graphics.ApplyChanges();
+
+            _heightMap = MapGenerator.GenerateNoiseMap(new Random().Next());
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _pixel = Content.Load<Texture2D>("images/pixel");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            KeyboardState keyState = Keyboard.GetState();
+            
+            if (keyState.IsKeyDown(Keys.Escape)) Exit();
+            
+            Camera.Update(keyState);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+
+            _spriteBatch.Begin();
+
+            Camera.DrawMap(_spriteBatch, _pixel, _heightMap);
+            
+            _spriteBatch.End();
+            
+            base.Draw(gameTime);
+        }
+    }
+}
